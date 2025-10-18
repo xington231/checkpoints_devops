78c6e76663da83e200cb05e5e3a9f672787dc17d
Алина <degtyareva_18@mail.ru>
2025-10-18 22:57:49 +0300

    update

--- calculator/ConsoleApp60/Program.cs
+++ calculator/ConsoleApp60/Program.cs
@@ -39,20 +39,10 @@ namespace Calculator
 
     }
 
-    internal static class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Calculator.Power(4, 2));
-            Console.WriteLine(Calculator.Add(4, 2));
-            Console.WriteLine(Calculator.Multiply(4, 2));
-            Console.WriteLine(Calculator.Divide(4, 2));
-            Console.WriteLine(Calculator.Subtract(4, 2));
-            Console.WriteLine($"Divide(4, 0) = {Calculator.Divide(4, 0)}");
-            Console.WriteLine(Calculator.Power(4, 0));
-            Console.WriteLine(Calculator.Power(0, 3));
-            Console.WriteLine($"Power(0, -2) = {Calculator.Power(0, -2)}");
-
         }
     }
 }
